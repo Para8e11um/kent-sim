8d5ee5b2a766a8b660848163e3bd5e5ed19944cf
para8e11um <para8e11um1@gmail.com>
2026-09-18 12:55:36 +0600

    Add biome message generator

--- agent.py
+++ agent.py
@@ -12,10 +12,10 @@ class PersonAgent(Agent):
 
     # Метод получения информации о соседях на клетке(возвращает предложение о соседях)
     def get_neighbors_message(self):
-        message = "Неподалёку от вас находятся: "
+        message = " Неподалёку от вас находятся: "
         neighbors = [a for a in self.model.grid.get_neighbors(self.pos, moore =True, radius = 0, include_center=True) if a.agent_type == "Person"]
         if len(neighbors) == 1:
-            message = "Вы одни в окрестности."
+            message = " Вы одни в окрестности."
             return message
         for neighbor in neighbors:
             if neighbor.unique_id != self.unique_id:
@@ -23,6 +23,19 @@ class PersonAgent(Agent):
         message = message.removesuffix(", ")
         message += "."
         return message
+
+    # Метод получения информации о биоме(возвращает предложение о биоме)
+    def get_biome_message(self):
+        local_biome = self.model.biome_map.get_biome(self.pos[0],self.pos[1])["name"]
+        match local_biome:
+            case "Поле":
+                return " Вы находитесь в полях."
+            case "Лес":
+                return " Вы находитесь в лесу."
+            case "Город":
+                return " Вы находитесь в руинах города."
+            case _:
+                return ""
 
     def step(self):
         """Действие агента на каждом шаге (пока только случайное движение)"""
